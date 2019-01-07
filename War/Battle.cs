beaf5117330ec898804218e3af5d7f8677682206
Tim <talbert1209@gmail.com>
2019-01-06 21:23:58 -0600

    Completed first pass.

--- War/Battle.cs
+++ War/Battle.cs
@@ -32,12 +32,14 @@ namespace War
             if (GetBattleWinner() == "player1")
             {
                 _player1.AddToBountyCount(2);
-                _label.Text += "<b>Player 1 Wins!</b></br>";
+                DisplayBounty();
+                _label.Text += "<b>Player 1 Wins!</b></br></br>";
             }
             else if (GetBattleWinner() == "player2")
             {
                 _player2.AddToBountyCount(2);
-                _label.Text += "<b>Player 2 Wins!</b></br>";
+                DisplayBounty();
+                _label.Text += "<b>Player 2 Wins!</b></br></br>";
             }
             else if (GetBattleWinner() == "war")
             {
@@ -47,7 +49,39 @@ namespace War
 
         private void War()
         {
-            _label.Text += "******** WAR ********</br>";
+            var winner = false;
+            List<Card> warBounty = new List<Card>();
+            while (!winner)
+            {
+                _label.Text += "******** WAR ********</br></br>";
+                warBounty.AddRange(_player1.PlayCard(2));
+                warBounty.AddRange(_player2.PlayCard(2));
+
+                var player1WarBattleCard = _player1.PlayCard();
+                var player2WarBattleCard = _player2.PlayCard();
+
+                warBounty.Add(player1WarBattleCard);
+                warBounty.Add(player2WarBattleCard);
+
+                Bounty.AddRange(warBounty);
+
+
+                DisplayBounty();
+
+                if (player1WarBattleCard.Value > player2WarBattleCard.Value)
+                {
+                    _player1.AddToBountyCount(warBounty.Count + 2);
+                    _label.Text += "<b>Player 1 Wins!</b></br></br>";
+                    winner = true;
+                }
+
+                if (player1WarBattleCard.Value < player2WarBattleCard.Value)
+                {
+                    _player2.AddToBountyCount(warBounty.Count + 2);
+                    _label.Text += "<b>Player 2 Wins!</b></br></br>";
+                    winner = true;
+                }
+            }
         }
 
         private string GetBattleWinner()
@@ -57,6 +91,15 @@ namespace War
             if (_player1BattleCard.Value < _player2BattleCard.Value)
                 return "player2";
             return "war";
+        }
+
+        private void DisplayBounty()
+        {
+            _label.Text += $"Bounty...</br>";
+            foreach (var card in Bounty)
+            {
+                _label.Text += $"&nbsp;&nbsp;&nbsp;{card.Name}</br>";
+            }
         }
     }
 }
--- War/Game.cs
+++ War/Game.cs
@@ -54,71 +54,22 @@ namespace War
             for (int i = 0; i < 20; i++)
             {
                 Battle battle = new Battle(_player1, _player2, _label);
-
-
-
-
-
-                //var player1Card = PlayCard(_player1);
-                //var player2Card = PlayCard(_player2);
-                //List<Card> bounty = new List<Card>(){player1Card, player2Card};
-
-                //if (player1Card.Value > player2Card.Value)
-                //{
-                //    _label.Text += $"Battle Cards: {player1Card.Name} vs {player2Card.Name}</br>";
-                //    _label.Text += $"{GetBounty(bounty)}";
-                //    _label.Text += "<b>Player 1 Wins!</b></br>";
-                //}
-                //else if (player2Card.Value > player1Card.Value)
-                //{
-                //    _label.Text += $"Battle Cards: {player1Card.Name} vs {player2Card.Name}</br>";
-                //    _label.Text += $"{GetBounty(bounty)}";
-                //    _label.Text += "<b>Player 2 Wins</b></br>";
-                //}
-                //else
-                //{
-                //    _label.Text += $"Battle Cards: {player1Card.Name} vs {player2Card.Name}</br>";
-                //    bounty.AddRange(GetWarBounty());
-                //    War(player1Card, player2Card);
-                    
-                //    _label.Text += "<b>y'all tie</b></br>";
-                //}
             }
+            GetWinner();
         }
 
-        private string GetBounty(List<Card> bountyCards)
+        private void GetWinner()
         {
-            var bountyString = "Bounty...</br>";
-            foreach (var card in bountyCards)
-            {
-                bountyString += $"&nbsp;&nbsp;&nbsp;{card.Name}</br>";
-            }
+            _label.Text += "<b>Battle Stats...</b></br>";
+            _label.Text += $"Player 1: {_player1.BountyCount} Bounties</br>";
+            _label.Text += $"Player 2: {_player2.BountyCount} Bounties</br></br>";
 
-            return bountyString;
+            if (_player1.BountyCount > _player2.BountyCount)
+                _label.Text += "<b>Player 1 Wins!</b>";
+            else if (_player1.BountyCount < _player2.BountyCount)
+                _label.Text += "<b>Player 2 Wins!</b>";
+            else
+                _label.Text += "<b>Tie Game!</b>";
         }
-
-        private void War(Card player1Card, Card player2Card)
-        {
-            List<Card> bountyCards = new List<Card>(){player1Card, player2Card};
-            Card player1Burn1 = _player1.PlayCard();
-            Card player2Burn1 = _player2.PlayCard();
-            Card player1Burn2 = _player1.PlayCard();
-            Card player2Burn2 = _player2.PlayCard();
-            Card player1BattleCard = _player1.PlayCard();
-            Card player2BattleCard = _player2.PlayCard();
-
-            _label.Text += "********WAR********</br>";
-
-
-        }
-
-        private List<Card> GetWarBounty()
-        {
-            List<Card> warBounty = new List<Card>();
-            warBounty.AddRange(PlayCards(_player1, 2));
-            warBounty.AddRange(PlayCards(_player2, 2));
-            return warBounty;
-        }
-       
     }
 }
